35edb0653332a6c65d6bcaceb5d439862848016f
Gisely Farias <giselyfarias7@gmail.com>
2023-06-27 09:00:42 -0300

    Atividade Balonismo balao main pessoa piloto

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,2 +1,49 @@
 public class Pessoa {
+
+    String nome;
+
+    String sobrenome;
+
+    String genero;
+
+    double peso;
+
+    double altura;
+
+    int idade;
+
+    Pessoa Pessoa = new Pessoa();
+
+    public Pessoa(){
+    }
+    public String nome(){
+        System.out.println("Henrique");
+        return nome = nome;
+    }
+
+    public String sobrenome() {
+        System.out.println("Silva");
+        return sobrenome = sobrenome;
+    }
+
+    public String genero(){
+        System.out.println("masculino");
+        return genero = genero;
+    }
+
+    public double peso(){
+        System.out.println("58,3kg");
+        return peso = peso;
+    }
+
+    public double altura(){
+        System.out.println("1.80");
+        return altura = altura;
+    }
+
+    public int idade(){
+        System.out.println("40 anos");
+        return idade = idade;
+    }
+
 }
--- src/Piloto.java
+++ src/Piloto.java
@@ -1,2 +1,14 @@
 public class Piloto {
+    public static void main(String[] args) {
+        //Construir balões
+        Piloto p1 = new Piloto();
+        String resultado = p1.pilotar();
+        System.out.println(resultado);
+    }
+    public String pilotar(){
+        return "pilotando";
+    }
 }
+
+
+
